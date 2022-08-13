c04ec6b22da6bebed2e62f447625712390b50c72
Newpersonhere <97367726+Newpersonhere@users.noreply.github.com>
2022-08-13 13:40:11 -0400

    Update goomba.inc.c

--- src/game/behaviors/goomba.inc.c
+++ src/game/behaviors/goomba.inc.c
@@ -12,9 +12,9 @@
 static struct ObjectHitbox sGoombaHitbox = {
     /* interactType:      */ INTERACT_BOUNCE_TOP,
     /* downOffset:        */ 0,
-    /* damageOrCoinValue: */ 1,
+    /* damageOrCoinValue: */ 0,
     /* health:            */ 0,
-    /* numLootCoins:      */ 1,
+    /* numLootCoins:      */ 999,
     /* radius:            */ 72,
     /* height:            */ 50,
     /* hurtboxRadius:     */ 42,
@@ -78,7 +78,7 @@ void bhv_goomba_triplet_spawner_update(void) {
 
     // If mario is close enough and the goombas aren't currently loaded, then
     // spawn them
-    if (o->oAction == GOOMBA_TRIPLET_SPAWNER_ACT_UNLOADED) {
+    if (o->oAction == GOOMBA_TRIPLET_SPAWNER_ACT_LOADED) {
         if (o->oDistanceToMario < 3000.0f * configDrawDistanceMultiplier || configDisableDrawDistance) {
             // The spawner is capable of spawning more than 3 goombas, but this
             // is not used in the game
